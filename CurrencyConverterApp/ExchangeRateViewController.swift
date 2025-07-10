955efedce9210051699f56e2d45cf831b1d96575
Pjh01 <estelle0102@naver.com>
2025-07-10 10:46:45 +0900

    Add: project file

---
+++ CurrencyConverterApp/ExchangeRateViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyConverterApp
+//
+//  Created by estelle on 7/10/25.
+//
+
+import UIKit
+
+class ExchangeRateViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    view.backgroundColor = .systemBackground
+  }
+
+
+}
+
